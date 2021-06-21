18d1b4f6f0d94971095c1ed098d6a26c893fca25
Kevin J. Dugan <dugankj@ornl.gov>
2021-06-21 18:36:51 -0400

    Moving to node server

---
+++ main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+const Vision = require("@hapi/vision");
+const Inert = require("@hapi/inert");
+const os = require("os");
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: "localhost"
+    });
+
+    server.route([
+        {
+            method: "GET",
+            path: "/",
+            handler: (request, h) => {
+
+                return h.view("mainPage", {hostName: `${os.hostname}`});
+            }
+        },
+        {
+            method: "GET",
+            path: "/fireflies",
+            handler: (request, h) => {
+
+                return h.view("fireflies", {hostName: `${os.hostname}`});
+            }
+        },
+        {
+            method: "GET",
+            path: "/css/{file}",
+            handler: (request, reply) => {
+                return reply.file("./css/" + request.params.file);
+            }
+        },
+        {
+            method: "GET",
+            path: "/script/{file}",
+            handler: (request, reply) => {
+                return reply.file("./script/" + request.params.file);
+            }
+        }
+    ]);
+
+    await server.start();
+    await server.register([Vision, Inert]);
+    server.views({
+        engines: {
+            html: require("handlebars")
+        },
+        relativeTo: __dirname,
+        path: "templates"
+    });
+    console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
